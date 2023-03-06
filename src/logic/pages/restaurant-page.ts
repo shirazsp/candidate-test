989082dd7dc8ba830f3ba5c28111cd80d18dfa79
Shiraz <shiraz.almog@gmail.com>
2023-03-07 01:35:49 +0200

    add UI tests

--- src/logic/pages/restaurant-page.ts
+++ src/logic/pages/restaurant-page.ts
@@ -2,7 +2,21 @@ import { Page } from "playwright";
 import { PageBase } from "./page-base";
 
 const CREATE_NEW_RESTURANT_BUTTON = "//button[contains(text(),'Create new')]";
-const POPUP_TITLE = "//h2[contains(text(),'Create new restaurant')]"
+const DELETE_RESTAURANT_BUTTON = function (x: number) {
+    return "//td[2][contains(text(),'" + x.toString() + "')]/following-sibling::td/button"
+};
+const RESTAURANT_ID_TD = function (x: number) { return "//td[2][contains(text(),'" + x.toString() + "')]" };
+
+const POPUP_TITLE = "//h2[contains(text(),'Create new restaurant')]";
+const POPUP_ID_FIELD = "//input[@id='id']";
+const POPUP_NAME_FIELD = "//input[@id='name']";
+const POPUP_ADDRESS_FIELD = "//input[@id='address']";
+const POPUP_SCORE_FIELD = "//input[@id='score']";
+const POPUP_SUBMIT_BUTTON = "//button[contains(text(),'Submit')]";
+
+const CREATED_POPUP_TITLE = "//h2[contains(text(),'Created!')]";
+const CREATED_POPUP_OK_BUTTON = "//div[@id='alert-popup']/*/button[contains(text(),'OK')]";
+
 
 export class RestaurantPage extends PageBase {
 
@@ -16,4 +30,49 @@ export class RestaurantPage extends PageBase {
     checkIfTitleInPopupExcit = async () => {
         return await this.page.isVisible(POPUP_TITLE);
     }
+    fillPopupFieldsAndSubmit = async (id: number, name: string, address: string, score: number) => {
+        let inputField;
+        //fill 'id' field
+        inputField = this.page.locator(POPUP_ID_FIELD);
+        await inputField.fill(id.toString());
+        //fill 'name' field
+        inputField = this.page.locator(POPUP_NAME_FIELD);
+        await inputField.fill(name);
+        //fill 'address' field
+        inputField = this.page.locator(POPUP_ADDRESS_FIELD);
+        await inputField.fill(address);
+        //fill 'score' field
+        inputField = this.page.locator(POPUP_SCORE_FIELD);
+        await inputField.fill(score.toString());
+        //click 'submit' button
+        await this.page.click(POPUP_SUBMIT_BUTTON);
+    }
+    checkIfCreatedPopupTitleExist = async () => {
+        return await this.page.isVisible(CREATED_POPUP_TITLE);
+    }
+    clickOkButtonInCreatedPopup = async () => {
+        await this.page.click(CREATED_POPUP_OK_BUTTON);
+    }
+    deleteRestaurantById = async (id: number) => {
+        await this.page.click(DELETE_RESTAURANT_BUTTON(id));
+    }
+    getRestaurantByID = async (id: number) => {
+        const count = await this.page.locator(RESTAURANT_ID_TD(id)).count();
+        if (count > 0) {
+            const idElement = await this.page.locator(RESTAURANT_ID_TD(id));
+            const nameElement = await this.page.locator(RESTAURANT_ID_TD(id) + "/following-sibling::td[1]");
+            const addressElement = await this.page.locator(RESTAURANT_ID_TD(id) + "/following-sibling::td[2]");
+            const scoreElement = await this.page.locator(RESTAURANT_ID_TD(id) + "/following-sibling::td[3]");
+
+            return {
+                id: Number(await idElement.textContent()),
+                name: await nameElement.textContent(),
+                address: await addressElement.textContent(),
+                score: Number(await scoreElement.textContent())
+            };
+        }
+        return null;
+    };
+
+
 }
--- src/tests/restaurant-ui-api.spec.ts
+++ src/tests/restaurant-ui-api.spec.ts
@@ -18,14 +18,48 @@ describe('UI-API tests', () => {
         resturantPage = await browser.newPage(RestaurantPage, configJson.baseUiUrl);
     })
 
-    // afterEach('Close browser', async () => {
-    //     await browser.close();
-    // })
+    afterEach('Close browser', async () => {
+        await browser.close();
+    })
 
-    it('Create a new Restaurant and delete it', async function () {
+    it.only('Create a new Restaurant and delete it', async function () {
+        //Arrange
+        let restaurants: ApiResponse<Restaurant[]> = await restaurantsAPI.getRestaurants();
+        const initialAmount = restaurants.data?.length;
+        const newRestaurantId: number = 777;
+
+        //Act
+        //create a new restaurante
         await resturantPage.clickreateNewRestaurantButtone();
-        let actualResult = await resturantPage.checkIfTitleInPopupExcit();
-        expect(actualResult, 'Restaurants popup was not opened').to.be.true;
+        await resturantPage.checkIfTitleInPopupExcit();
+        await resturantPage.fillPopupFieldsAndSubmit(
+            newRestaurantId,
+            "The Happy Malawach",
+            "Weizmann 135, Kfar Saba",
+            4.9)
+        await resturantPage.checkIfCreatedPopupTitleExist();
+        await resturantPage.clickOkButtonInCreatedPopup();
+
+        //delete the new restaurante
+        await resturantPage.deleteRestaurantById(newRestaurantId);
+        // add a small delay after the delete operation
+        await new Promise(r => setTimeout(r, 1000));
+        restaurants = await restaurantsAPI.getRestaurants();
+        const getRestaurantResult = await resturantPage.getRestaurantByID(newRestaurantId);
+        const getByIdResponse = await restaurantsAPI.getRestaurantById(newRestaurantId);
+
+
+        //Assert
+        //validate the actual restaurants amount equals to the initial amount (after deletion)
+        const actualAmount = restaurants.data?.length;
+        expect(actualAmount).to.equal(initialAmount, 'Restaurants amount is not as expected');
+        //validate that the deleted restaurant is missing from the UI
+        expect(getRestaurantResult, 'Restaurant exist after it been deleted').to.be.null;
+
+        //validate that the deleted restaurant is missing using the API
+        expect(getByIdResponse.success, "'Restaurant exist after it been deleted'").to.be.false;
+        expect(getByIdResponse.status).to.equal(404);
+
     })
 })
 
